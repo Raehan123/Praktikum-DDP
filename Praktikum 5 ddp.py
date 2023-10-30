ddf97ee218db5762e53a32a907dba9de6ca27423
Raehan123 <145969850+Raehan123@users.noreply.github.com>
2023-10-30 11:10:35 +0700

    Add files via upload

---
+++ Praktikum 5 ddp.py
@@ -0,0 +1,25 @@
+motor = ["F2110G","Vario","150","hijau"]
+motor.append( "15.000.000")
+motor.append( "2")
+
+motor.insert(2, "Honda")
+motor.insert(3, "Matic")
+
+kodekendaraan = (motor[0])
+print("kode kendaraan  : ",kodekendaraan)
+namakendaraan = (motor[1])
+print("nama kendaraan  : ",namakendaraan)
+merkkendaraan = (motor[2])
+print("merk kendaraan  : ",merkkendaraan)
+jeniskendaraan = (motor[3])
+print("jenis kendaraan : ",jeniskendaraan)
+cckendaraan = (motor[4])
+print("cc kendaraan    : ",cckendaraan)
+warnakendaraan = (motor[5])
+print("warna kendaraan : ",warnakendaraan)
+hargakendaraan = (motor[6])
+print("harga kendaraan : ",hargakendaraan)
+jumlahroda = (motor[7])
+print("jumlah roda     : ",jumlahroda)
+
+ 
